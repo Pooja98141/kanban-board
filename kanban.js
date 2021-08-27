f93c9e88366057ead83eb1d283cc07aff1cb0178
Pooja98141 <89545722+Pooja98141@users.noreply.github.com>
2021-08-27 20:46:05 +0530

    Add files via upload

---
+++ kanban.js
@@ -0,0 +1,58 @@
+const dragStart = target => {
+    target.classList.add('dragging');
+};
+
+const dragEnd = target => {
+    target.classList.remove('dragging');
+};
+
+const dragEnter = event => {
+    event.currentTarget.classList.add('drop');
+};
+
+const dragLeave = event => {
+    event.currentTarget.classList.remove('drop');
+};
+
+const drag = event => {
+    event.dataTransfer.setData('text/html', event.currentTarget.outerHTML);
+    event.dataTransfer.setData('text/plain', event.currentTarget.dataset.id);
+};
+
+const drop = event => {
+    document.querySelectorAll('.column').forEach(column => column.classList.remove('drop'));
+    document.querySelector(`[data-id="${event.dataTransfer.getData('text/plain')}"]`).remove();
+
+    event.preventDefault();
+    event.currentTarget.innerHTML = event.currentTarget.innerHTML + event.dataTransfer.getData('text/html');
+};
+
+const allowDrop = event => {
+    event.preventDefault();
+};
+
+document.querySelectorAll('.column').forEach(column => {
+    column.addEventListener('dragenter', dragEnter);
+    column.addEventListener('dragleave', dragLeave);
+});
+
+document.addEventListener('dragstart', e => {
+    if (e.target.className.includes('card')) {
+        dragStart(e.target);
+    }
+});
+
+document.addEventListener('dragend', e => {
+    if (e.target.className.includes('card')) {
+        dragEnd(e.target);
+    }
+});
+
+    
+function addTask(){
+let newT =document.getElementById('taskText').value;
+console.log(newT);
+document.getElementById('newTasks').innerHTML+=`<article class="card" draggable="true" ondragstart="drag(event)" data-id="3">
+<h3>${newT}</h3></article>`;
+}
+ 
